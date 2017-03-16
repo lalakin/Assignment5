6ab119915eaf82fc3418dd7ff97248efea202d89
lalakin <sofiamartin11@gmail.com>
2017-03-16 14:50:01 -0700

    vnbvnv
    vmmbm

---
+++ script.js
@@ -0,0 +1,80 @@
+//STEP1
+//function writeMessage() {
+//	alert("I have been clicked");
+//}
+
+//STEP2
+//var a = document.getElementById("mybutton");
+//function writeMessage() {
+//	alert("I have been clicked");
+//}
+
+//STEP3
+//var a = document.getElementById("myBtn");
+//function writeMessage(){
+//	alert("I have been clicked");
+//}	
+//a.addEventListener("click", writeMessage);
+
+//STEP4
+//var a = document.getElementById("myBtn");
+//a.addEventListener("click", function(){
+//	alert("I have been clicked");
+//});
+
+//STEP5
+//document.addEventListener("DOMContentLoaded", init, false);
+//function init() {
+//	myBtn.addEventListener("click", writeMessage, false);
+//}
+//function writeMessage() {
+//	alert("I have been clicked");
+//}
+
+//STEP6
+//function writeMessage() {
+//	alert("Click on the link");
+//}
+//writeMessage();
+//document.getElementById("redirect").addEventListener("click", 
+//function(event){
+//	event.preventDefault()
+//}, false);
+
+//STEP7
+// document.getElementById("Btn").addEventListener("click", function(){
+//	Btn.disabled = true;
+//	var box = document.getElementById("tBox");
+//alert(box.value);
+//});
+
+//STEP8
+//document.getElementById("Btn").addEventListener("click", function() {
+//	window.open('http://127.0.0.1:58000/newpage.html', 'np', 'width=300, height=300').resizeTo(500,300).focus();
+//});
+//	//function resizeMe() {
+//	//	window.resizeTo(500,300);
+//	//	window.focus();
+
+
+//STEP9
+//var a;
+//document.getElementById("Btn1").addEventListener("click", function start() {
+//	console.log("Learning JavaScript is fun!");
+//	 a = setInterval(start, 2000);
+//});
+//
+//document.getElementById("Btn2").addEventListener("click", function stop() {
+//	clearInterval(a);
+//});
+
+//STEP10
+//document.getElementById("Btn").addEventListener("click", function() {
+//	var x = document.getElementById("drop").selectedIndex;
+//	var y = document.getElementById("drop").options;
+//	alert(y[x].text)
+//});
+
+
+//The Calculator
+
